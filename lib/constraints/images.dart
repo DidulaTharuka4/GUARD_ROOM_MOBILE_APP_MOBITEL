339aeebf7a20c67cbb5850887ac651c89dde1b59
DidulaTharuka4 <didulat00@gmail.com>
2024-09-09 16:26:42 +0530

    create images string file

---
+++ lib/constraints/images.dart
@@ -0,0 +1,14 @@
+class ApplicationImages { 
+  static const String calender = 'assets/images/Calender.svg';
+  static const String logo = 'assets/images/SLTMobitel_Logo.svg';
+  static const String logoutIcon = 'assets/images/logout-box-line.svg';
+  static const String backIcon = 'assets/images/BackIcon.svg';
+
+  static const String selectorButtonDots = 'assets/images/selector_dots.svg';
+  static const String selectorButtonArrow = 'assets/images/Next_Arrow.svg';
+  static const String threeDotsMenuIcon = 'assets/images/Next_Arrow.svg';
+
+  static const String warningIcon = 'assets/images/warning.svg';
+  static const String successIcon = 'assets/images/sucess.svg';
+  static const String failedIcon = 'assets/images/warning.svg';
+}
