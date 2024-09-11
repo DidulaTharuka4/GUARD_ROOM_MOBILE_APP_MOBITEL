04f84c1515fdf16381a5bde6eea94ed097de9009
DidulaTharuka4 <didulat00@gmail.com>
2024-09-11 16:51:33 +0530

    main button modified

---
+++ lib/components/main_button.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter/widgets.dart';
+
+class CustomButton extends StatelessWidget {
+  final String innerText;
+  final Color backgroundColor;
+  final VoidCallback onPress;
+  final TextStyle textStyles;
+  final double buttonWidth;
+  final double buttonHeight;
+  final Color borderColor;
+  final double borderWidth;
+  final double borderRadius;
+
+  const CustomButton(
+      {Key? key,
+      required this.innerText,
+      required this.backgroundColor,
+      required this.onPress,
+      required this.textStyles,
+      required this.buttonWidth,
+      required this.buttonHeight,
+      required this.borderColor,
+      this.borderWidth  = 0.0,
+      required this.borderRadius})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPress,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: backgroundColor,
+        minimumSize: Size(buttonWidth, buttonHeight),
+        side: BorderSide(
+          color: borderColor,
+          width: borderWidth,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(borderRadius),
+        ),
+      ),
+      child: Text(innerText, style: textStyles),
+    );
+  }
+}
