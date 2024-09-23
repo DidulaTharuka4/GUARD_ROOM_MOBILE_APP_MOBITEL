ab18b43e5172032ef5e27e93b6ddbc117288fb16
DidulaTharuka4 <didulat00@gmail.com>
2024-09-24 02:18:34 +0530

    all buttons

---
+++ lib/components/Buttons/main_button.dart
@@ -0,0 +1,54 @@
+import 'package:Guard_Room_Application/constraints/colors.dart';
+import 'package:Guard_Room_Application/constraints/textSizes.dart';
+import 'package:flutter/material.dart';
+// import 'package:flutter/widgets.dart';
+
+class CustomButton extends StatelessWidget {
+  final String innerText;
+  final Color backgroundColor;
+  final VoidCallback onPress;
+  // final TextStyle textStyles;
+  final double buttonWidth;
+  final double buttonHeight;
+  final Color borderColor;
+  final double borderWidth;
+  // final double borderRadius;
+
+  const CustomButton(
+      {Key? key,
+      required this.innerText,
+      required this.backgroundColor,
+      required this.onPress,
+      // required this.textStyles,
+      required this.buttonWidth,
+      required this.buttonHeight,
+      required this.borderColor,
+      this.borderWidth = 0.0,
+      // required this.borderRadius
+      })
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPress,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: backgroundColor,
+        minimumSize: Size(buttonWidth, buttonHeight),
+        side: BorderSide(
+          color: borderColor,
+          width: borderWidth,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4),
+        ),
+      ),
+      child: Text(innerText,
+          style: const TextStyle(
+              fontSize: ApplicationTextSizes.LoginButtonTitleValue,
+              fontWeight: ApplicationTextWeights.LoginButtonTitleWeight,
+              fontFamily: 'Poppins',
+              color: ApplicationColors.PURE_WHITE)),
+    );
+  }
+}
---
+++ lib/components/Buttons/toggle_button.dart
@@ -0,0 +1,90 @@
+import 'package:Guard_Room_Application/constraints/colors.dart';
+import 'package:flutter/material.dart';
+// import 'package:sample_flutter_application_1/constraints/colors.dart';
+
+class CustomToggleButton extends StatefulWidget {
+  // final Color backgroundColor;
+  // final Color dotColor;
+  final VoidCallback changeToggleAction;
+
+  CustomToggleButton( 
+      {Key? key,
+      // required this.backgroundColor,
+      // required this.dotColor,
+      required this.changeToggleAction,
+      })
+      : super(key: key);
+  @override
+  _CustomToggleButton createState() => _CustomToggleButton();
+}
+
+class _CustomToggleButton extends State<CustomToggleButton> {
+
+  bool isToggled = false;
+
+  late Color backgroundColor;
+  late Color dotColor;
+  late VoidCallback changeToggleAction;
+
+  void initState() {
+    super.initState();
+    // backgroundColor = widget.backgroundColor;
+    // dotColor = widget.dotColor;
+    changeToggleAction = widget.changeToggleAction;
+  }
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          isToggled = !isToggled;
+        });
+        changeToggleAction();
+      },
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        width: 60,
+        height: 28,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          // color: ApplicationColors.BUTTON_COLOR_BLUE,
+          // color: ApplicationColors.TOGGLE_BUTTON_OFF_COLOR,
+          color: isToggled ? ApplicationColors.BUTTON_COLOR_BLUE : ApplicationColors.TOGGLE_BUTTON_OFF_COLOR,
+        ),
+        child: Stack(
+          children: <Widget>[
+            AnimatedPositioned(
+              duration: Duration(milliseconds: 300),
+              curve: Curves.easeIn,
+              left: isToggled ? 30.0 : 0.0,
+              right: isToggled ? 0.0 : 30.0,
+              child: InkWell(
+                onTap: () {
+                  setState(() {
+                    isToggled = !isToggled;
+                  });
+                  changeToggleAction();
+                },
+                child: AnimatedSwitcher(
+                  duration: Duration(milliseconds: 300),
+                  transitionBuilder:
+                      (Widget child, Animation<double> animation) {
+                    return ScaleTransition(child: child, scale: animation);
+                  },
+                  child: Icon(
+                          Icons.circle,
+                          color: ApplicationColors.PURE_WHITE,
+                          size: 26.0,
+                          key: UniqueKey(),
+                        )
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
