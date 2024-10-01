7ba25b44f35807ff8188b7b9035335a42dd4a300
DidulaTharuka4 <didulat00@gmail.com>
2024-10-01 09:29:41 +0530

    reusable text field

--- lib/components/main_text_input.dart
+++ lib/components/main_text_input.dart
@@ -1,64 +1,3 @@
-// import 'package:Guard_Room_Application/constraints/colors.dart';
-// import 'package:Guard_Room_Application/constraints/textSizes.dart';
-// import 'package:flutter/material.dart';
-
-// class CustomTextInput extends StatelessWidget {
-//   final String titleText;
-//   final TextStyle textStyles;
-//   final TextEditingController inputController;
-//   final TextInputType keyboardType;
-//   final int maxCharacterLength;
-
-//   const CustomTextInput({
-//     Key? key,
-//     required this.titleText,
-//     required this.textStyles,
-//     required this.inputController,
-//     required this.keyboardType,
-//     required this.maxCharacterLength,
-//   }) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       child: Row(
-//         children: <Widget>[
-//           Expanded(
-//             flex: 1,
-//             child: Text(
-//               titleText,
-//               style: const TextStyle(
-//                   fontSize: ApplicationTextSizes.userInputFieldLabelValue,
-//                   fontFamily: 'Poppins',
-//                   fontWeight: ApplicationTextWeights.UserInputsLabelWeight),
-//             ),
-//           ),
-//           SizedBox(width: 10),
-//           Expanded(
-//             flex: 1,
-//             child: TextFormField(
-//               controller: inputController,
-//               validator: (value) {
-//                 if (value == null || value.isEmpty) {
-//                   return 'This field is required';
-//                 }
-//                 return null;
-//               },
-//               maxLength: maxCharacterLength,
-//               decoration: InputDecoration(
-//                 filled: true,
-//                 fillColor: ApplicationColors.PURE_WHITE,
-//                 border: OutlineInputBorder(),
-//                 // hintText: 'Enter text here',
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
 import 'package:Guard_Room_Application/constraints/colors.dart';
 import 'package:Guard_Room_Application/constraints/marginValues.dart';
 import 'package:Guard_Room_Application/constraints/textSizes.dart';
@@ -69,22 +8,32 @@ class CustomTextInput extends StatelessWidget {
   final String titleText;
   final TextEditingController inputController;
   final bool inputEnabled;
-  final String? inputTextError;
+  // final String? inputTextError;
   final TextInputType keyboardType;
   // final int maxCharacterLength;
   final List<TextInputFormatter>? inputFormatters;
   final ValueChanged<String>? onChange;
+  final bool buttonClickStatus;
+  final bool isRequired;
+  final bool obscureText;
+  final VoidCallback? onTap;
+  // final bool controllerStatus;
 
   const CustomTextInput({
     Key? key,
     required this.titleText,
     required this.inputController,
     this.inputEnabled = true,
-    this.inputTextError = '',
+    // this.inputTextError = '',
     this.keyboardType = TextInputType.text,
     // required this.maxCharacterLength,
     this.inputFormatters,
     this.onChange,
+    this.buttonClickStatus = false,
+    required this.isRequired,
+    this.obscureText = false,
+    this.onTap,
+    // this.controllerStatus = false,
   }) : super(key: key);
 
   @override
@@ -102,10 +51,11 @@ class CustomTextInput extends StatelessWidget {
                     // fontFamily: 'Poppins',
                     fontWeight: ApplicationTextWeights.UserInputsLabelWeight),
               ),
-              const Text(
-                "*",
-                style: TextStyle(
+              Text(
+                isRequired ? "*" : '',
+                style: const TextStyle(
                     fontSize: ApplicationTextSizes.userInputFieldLabelValue,
+                    // color: ApplicationColors.RED_COLOR,
                     color: ApplicationColors.RED_COLOR,
                     fontWeight: FontWeight.bold),
               ),
@@ -119,10 +69,11 @@ class CustomTextInput extends StatelessWidget {
                   child: TextField(
                     controller: inputController,
                     enabled: inputEnabled,
+                    obscureText: obscureText,
                     keyboardType: keyboardType,
                     style: const TextStyle(
-                          color: ApplicationColors.PURE_BLACK,
-                        ),
+                      color: ApplicationColors.PURE_BLACK,
+                    ),
                     // inputFormatters: <TextInputFormatter>[
                     //   VehicleNumberTextInputFormatter(),
                     //   LengthLimitingTextInputFormatter(maxCharacterLength)
@@ -132,9 +83,32 @@ class CustomTextInput extends StatelessWidget {
                       filled: true,
                       fillColor: ApplicationColors.PURE_WHITE,
                       border: const OutlineInputBorder(),
-                      errorText: inputTextError
+                      // errorText: inputTextError
+                      // errorText: inputController.text.isEmpty && isRequired? 'mandatory fields (*) cannot be empty' : ''
+                      // errorText: inputController.text.isEmpty && buttonClickStatus? 'mandatory fields (*) cannot be empty' : ''
+                      errorText: buttonClickStatus
+                          ? inputController.text.isEmpty
+                              ? 'mandatory fields (*) cannot be empty'
+                              : ''
+                          : '',
+                      errorBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: buttonClickStatus
+                                ? inputController.text.isEmpty
+                                    ? ApplicationColors.RED_COLOR
+                                    : ApplicationColors.PURE_BLACK
+                                : ApplicationColors
+                                    .PURE_BLACK // Set to any color you want
+                            ),
+                      ),
+                      focusedErrorBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: ApplicationColors.PURE_BLACK,
+                        ),
+                      ),
                     ),
                     onChanged: onChange,
+                    onTap: onTap,
                   ),
                 ),
               ],
